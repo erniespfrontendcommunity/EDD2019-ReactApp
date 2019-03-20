2ef40fded81c5326423e285a00427263095edb28
Thalandor <thalandor1985@hotmail.com>
2019-03-20 23:26:13 +0100

    Updated to use public network

--- edd2019-react/src/components/AddCat.tsx
+++ edd2019-react/src/components/AddCat.tsx
@@ -3,13 +3,13 @@ import Web3 from 'web3';
 import * as catABI from "../contractsBin/cat.json";
 import { AbiItem } from 'web3-utils/types';
 
-const web3 = new Web3("ws://localhost:8545");
-
 
 class AddCat extends React.Component {
 
     private async addCat() {
-        const catAddress = "0x080cf7a545bddeeb1f3ab898801d5b68dd198408";
+        // Metamask insert web3 object into window object when the account is unlocked
+        var web3 = new Web3(((window as any).web3.currentProvider));  
+        const catAddress = "0xe62d3bfaadbc5373fb14e78c91f0405b5838b981";
         let contract = new web3.eth.Contract(catABI as Array<AbiItem>, catAddress);
         let accounts = await web3.eth.getAccounts();
         await contract.methods.addCat("catName1", 2, 2, 2, 2, 2, 2).send({ from: accounts[0], value: web3.utils.toWei("0.1", "ether"), gas: 600000 })
