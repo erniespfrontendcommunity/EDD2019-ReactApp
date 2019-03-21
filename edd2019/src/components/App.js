2cac229b356a4770eebaa232d5824e68418b88b9
Rodrigo Ezequiel Diaz <Rodrigo@Rodrigos-MacBook-Air.local>
2019-03-21 20:51:18 +0100

    initial commit 2.0

---
+++ edd2019/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Paul's magic
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
