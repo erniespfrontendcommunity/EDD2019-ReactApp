23f652c13e130e0c3adb560acc89a0a910381748
Thalandor <thalandor1985@hotmail.com>
2019-03-24 23:56:30 +0100

    Added classification, reset league and fix play leaguew

--- edd2019/src/components/Classification.js
+++ edd2019/src/components/Classification.js
@@ -3,14 +3,32 @@ import Web3 from 'web3';
 import leagueABI from "../contractsBin/league.json";
 import { Config } from "../config/Config";
 
+class ClassificationList{
+    constructor(address, name, points){
+        this.address = address;
+        this.name = name;
+        this.points = points;
+    }
+}
+
 class Classification extends React.Component {
 
     async getClassification() {
         // Metamask insert web3 object into window object when the account is unlocked
         var web3 = new Web3(window.web3.currentProvider);
         let contract = new web3.eth.Contract(leagueABI, Config.LeagueContractAddress);
-        let accounts = await web3.eth.getAccounts();
-        await contract.methods.playGame().send({ from: accounts[0], gas: 600000 });
+        let classification = [];
+        let numberOfParticipants = await contract.methods.getTotalParticipants().call();
+        for (var i = 0; i < numberOfParticipants; ++i) {
+            let participantAddress = await contract.methods.getParticipant(i).call();
+            let squadName = await contract.methods.getSquadName(participantAddress).call();
+            let points = await contract.methods.points(participantAddress).call();
+            classification.push(new ClassificationList(participantAddress, squadName, points));
+        }
+        classification.sort(function(memberA, memberB){
+            return memberB.points - memberA.points;
+        });
+        console.log(classification);
     }
 
     render() {
--- edd2019/src/config/Config.js
+++ edd2019/src/config/Config.js
@@ -1,4 +1,4 @@
 export const Config = {
     CatContractAddress: '0x16d8ef9a12f618d426df1bfe7735cd8a74803572',
-    LeagueContractAddress: '0x3aeb8c7e110d3abf46f4d2a6a947a730e70c8b1e'
+    LeagueContractAddress: '0xab47dd2947dda24cd07a02c9f54dd6327db03d57'
 };
