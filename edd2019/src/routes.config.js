33cf879f73fba2dca4dbc57f28f92b2003d6f7af
paul_melero <paulmelero@gmail>
2019-03-24 10:07:27 +0100

    Added little optimization and chunk names

--- edd2019/src/routes.config.js
+++ edd2019/src/routes.config.js
@@ -3,27 +3,27 @@ import React from 'react';
 export const routesMap = {
   HOME: {
     path: '/',
-    component: React.lazy(() => import('./pages/Lore')),
+    component: React.lazy(() => import( /* webpackChunkName: "Lore" */ /* webpackPrefetch: true */  './pages/Lore')),
     label: 'Lore'
   },
   CREATE_CAT: {
     path: '/createCat',
-    component: React.lazy(() => import('./pages/AddCat')),
+    component: React.lazy(() => import( /* webpackChunkName: "AddCat" */ './pages/AddCat')),
     label: 'Create Cat'
   },
   CAT_LIST: {
     path: '/catSquad',
-    component: React.lazy(() => import('./pages/CatSquad')),
+    component: React.lazy(() => import( /* webpackChunkName: "CatSquad" */ './pages/CatSquad')),
     label: 'Add Cats to Squad'
   },
   PLAY_LEAGUE: {
     path: '/playLeague',
-    component: React.lazy(() => import('./pages/PlayLeague')),
+    component: React.lazy(() => import( /* webpackChunkName: "PlayLeague" */ './pages/PlayLeague')),
     label: 'Play Cat League'
   },
   TEST: {
     path: '/test',
-    component: React.lazy(() => import('./pages/SandBox')),
+    component: React.lazy(() => import( /* webpackChunkName: "SandBox" */ './pages/SandBox')),
     label: 'Sandbox'
   },
 }
