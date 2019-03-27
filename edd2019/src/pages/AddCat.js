eb9cb40e179acbb64114d061f377079784edcb9b
diro <diro@erni-espana.es>
2019-03-27 16:13:17 +0100

    Removing debugger

--- edd2019/src/pages/AddCat.js
+++ edd2019/src/pages/AddCat.js
@@ -22,7 +22,7 @@ class AddCat extends React.Component {
         var web3 = new Web3(window.web3.currentProvider);
         let contract = new web3.eth.Contract(catABI, Config.CatContractAddress);
         let accounts = await web3.eth.getAccounts();
-        debugger;
+
         await contract.methods.addCat(
             this.state.CatName,
             this.state.CatStealth,
