106b32ca2128a9197f7fec77878a715ff0571ca3
diro <diro@erni-espana.es>
2019-03-27 09:33:40 +0100

    Removing AddSquad from addCat

--- edd2019/src/pages/AddCat.js
+++ edd2019/src/pages/AddCat.js
@@ -34,14 +34,6 @@ class AddCat extends React.Component {
             .send({ from: accounts[0], value: web3.utils.toWei("0.1", "ether"), gas: 600000 })
     }
 
-    async addSquad() {
-        // Metamask insert web3 object into window object when the account is unlocked
-        var web3 = new Web3(window.web3.currentProvider);
-        let contract = new web3.eth.Contract(catABI, Config.LeagueContractAddress);
-        let accounts = await web3.eth.getAccounts();
-        await contract.methods.addCat("squad1", 0, 1, 2).send({ from: accounts[0], gas: 600000 })
-    }
-
     // Todo change this for only one function
 
     handleNameChange = (event) => {
