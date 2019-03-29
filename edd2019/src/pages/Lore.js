4c29862e65286bf08cc49b080c4ddb543177f078
diro <diro@erni-espana.es>
2019-03-29 15:04:51 +0100

    Changing Lore

--- edd2019/src/pages/Lore.js
+++ edd2019/src/pages/Lore.js
@@ -15,7 +15,7 @@ class Lore extends Component {
                 <p>People affectionately refer to me as the Blockchain Professor.</p>
                 <p>This world... is inhabited far and wide by creatures called Blockchain Cats.</p>
                 <p>For some people, Blockchain Cats are pets. Other use them for battling.</p>
-                <p>As for myself... I stufy Blockchain Cats as a profession.</p>
+                <p>As for myself... I study Blockchain Cats as a profession.</p>
                 <p>Your very own Blockchain legend is about to unfold!</p>
                 <p>A world of dreams and adventures with Blockchain Cats awaits!</p>
                 <p>Let's go!</p>
@@ -25,21 +25,16 @@ class Lore extends Component {
               <img src={oak} className="img-oak" alt="oak" />
             </div>
           </div>
-          <h4>In order to complete your Blockchain adventure you need to implement two functionalities:</h4>
+          <h4>In order to complete your Blockchain adventure you need to implement a function:</h4>
           <br />
-          <ol>
-            <li>Implement a function that creates a cat into the blockchain based on the form on the <code>Create Cat tab</code>:
+          <ul>
+            <li>This function creates a cat into the blockchain based on the form on the <code>Create Cat tab</code>:
                 <ul>
-                  <li>Your function should take into account the values on the form on send them to the Blockchain.</li>
-                  <li>To start, go to <code>src/pages/CreateCatForm.js</code> fulfill the function called <code>addCat</code>.</li>
+                  <li>Your function should take into account the values on the form and send them to the Blockchain.</li>
+                  <li>To start, go to <code>src/pages/CreateCatForm.js</code> and fulfill the function called <code>addCat</code>.</li>
                 </ul>
             </li>
-            <li>Implement a function that gets all our precious cats from the Blockchain:
-                <ul>
-                    <li>To start, go to <code>src/pages/CatSquad.js</code> fulfill the function called <code>getCatList</code>.</li>
-                </ul>
-            </li>
-          </ol>
+          </ul>
         </div>
 
       </section>
